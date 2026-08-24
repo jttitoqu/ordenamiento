7699d8f7a594444e1332cc3de296f4173933ffb1
Johann Ttito <jttitoqu@emeal.nttdata.com>
2026-08-24 14:59:27 -0500

    feat: agregar mostrar mayor numero y menor numero

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿List<int> listaEnteros = new List<int> {5, 7, 2, 4};
+﻿List<int> listaEnteros = new List<int> {5, 7, 2, 4, 9, 1};
 
+MostrarNumeroMayorYMenor(listaEnteros);
 Console.WriteLine("El promedio es:" + CalcularPromedio(listaEnteros));
 
 static double CalcularPromedio(List<int> lista)
+
 {
     if(lista == null || lista.Count == 0)
     {
@@ -17,4 +19,32 @@ static double CalcularPromedio(List<int> lista)
     }
     
     return acumulador/lista.Count;
+}
+
+static void MostrarNumeroMayorYMenor(List<int> lista)
+{
+    int menor = lista[0];
+    int mayor = lista[0];
+    if(lista == null || lista.Count == 0)
+    {
+        throw new ArgumentException("La lista está vacía");
+    }
+
+
+    for (int i = 1; i < lista.Count; i++)
+    {    
+        if (lista[i] > mayor)//5 7 2 ,9 3
+        {
+            mayor = lista[i];
+        }
+    }
+
+    for (int i = 1; i < lista.Count; i++)
+    {    
+        if (lista[i] < menor)//5 7 2 ,9 3
+        {
+            menor = lista[i];
+        }
+    }
+    Console.WriteLine($"El numero menor es {menor} y el numero mayor es {mayor}");
 }
