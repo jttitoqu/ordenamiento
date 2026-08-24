6b33aa9b60bd1ae90cc8aef9a8290ada3626466b
Johann Ttito <jttitoqu@emeal.nttdata.com>
2026-08-24 12:30:40 -0500

    feat: agregar calcular promedio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> listaEnteros = new List<int> {5, 7, 2, 4};
+
+Console.WriteLine("El promedio es:" + CalcularPromedio(listaEnteros));
+
+static double CalcularPromedio(List<int> lista)
+{
+    if(lista == null || lista.Count == 0)
+    {
+        throw new ArgumentException("La lista está vacía");
+    }
+
+    double acumulador = 0;
+
+    foreach(int numero in lista)
+    {
+        acumulador = acumulador + numero;
+    }
+    
+    return acumulador/lista.Count;
+}
